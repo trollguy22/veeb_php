6c5d4cfe9dca89d54b669889a480e195a2c9dc15
trollguy22 <joel.purasson@khk.ee>
2019-02-13 12:36:36 +0200

    print - tekst echo abil

---
+++ php_alused/print/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joel.purasson
+ * Date: 13.02.2019
+ * Time: 12:14
+ */
+echo "Hello world!<br>";
+echo "Hello world!<br>";
+echo "Hello world!<br>";
+
+
