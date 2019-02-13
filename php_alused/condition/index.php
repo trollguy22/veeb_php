52d643019f659f23b01c855d59d5d2373fabcb57
trollguy22 <joel.purasson@khk.ee>
2019-02-13 14:34:30 +0200

    condition - if else

--- php_alused/condition/index.php
+++ php_alused/condition/index.php
@@ -3,6 +3,13 @@ $eesNimi = 'Joel';
 $pereNimi = 'Purasson';
 $vanus = 18;
 $kaal = 85;
+$sugu = 'mees';
+
+if($sugu == 'mees'){echo '<p style="color: blue;">';
+}
+else {
+   echo '<p style="color: red;">';
+}
 echo 'Minu nimi on '.$eesNimi.'<br>';
 echo 'Minu perenimi on '.$pereNimi.'<br>';
 echo 'Olen '.$vanus.' aastat vana<br>';
