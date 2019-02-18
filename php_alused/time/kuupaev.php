8adf4108f82704476d80f1f377b05cf933b3de1a
trollguy22 <joel.purasson@khk.ee>
2019-02-18 10:57:21 +0200

    kuupaeva eestistamine

--- php_alused/time/kuupaev.php
+++ php_alused/time/kuupaev.php
@@ -1,3 +1,9 @@
 <?php
-echo date('d.m.Y G:i' , time());
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
 
