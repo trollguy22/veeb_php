c9754d983576a7c303a7ef3e0bf259a3c36f851a
trollguy22 <joel.purasson@khk.ee>
2019-02-20 15:16:03 +0200

    peppa pig

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -5,7 +5,19 @@
        4,
     1.04
     );
-    var_dump($peppaPig);
-    echo '<pre>';
-    print_r($peppaPig);
-    echo '</pre>';
+echo $peppaPig[0].'<br>';
+echo $peppaPig[1].'<br>';
+echo $peppaPig[2].'<br>';
+echo $peppaPig[3].'<br>';
+
+echo '<hr>';
+
+for($i = 0; $i < count($peppaPig); $i++) {
+    echo $peppaPig[$i].'<br>';
+}
+echo '<hr>';
+
+foreach ($peppaPig as $element) {
+    echo $element.'<br>';
+
+}
