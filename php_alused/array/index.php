0362d4713263137c94a6e19977b02f1ad5ce443b
trollguy22 <joel.purasson@khk.ee>
2019-02-20 15:29:33 +0200

    geogre

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -1,23 +1,35 @@
 <?php
-    $peppaPig = array(
-       'Peppa',
-       'naine',
-       4,
-    1.04
-    );
-echo $peppaPig[0].'<br>';
-echo $peppaPig[1].'<br>';
-echo $peppaPig[2].'<br>';
-echo $peppaPig[3].'<br>';
+    $porsad = array(
+         array(
+            'Peppa',
+            'naine',
+            4,
+            1.04
+        ),
+        array(
+        'geogre',
+        'mees',
+        2,
+        0.95
+    )
+);
+echo $porsad[0][0].'<br>';
+echo $porsad[1][1].'<br>';
+echo $porsad[2][2].'<br>';
+echo $porsad[3][3].'<br>';
 
 echo '<hr>';
 
-for($i = 0; $i < count($peppaPig); $i++) {
-    echo $peppaPig[$i].'<br>';
+for($i = 0; $i < count($porsad); $i++) {
+    for($j = 0; $j < count($porsad[$i][$j]); $j++){
+        echo $porsad[$i][$j].'<br>';
+    }
 }
 echo '<hr>';
 
-foreach ($peppaPig as $element) {
-    echo $element.'<br>';
-
+foreach ($porsad as $porsas) {
+    foreach ($porsas as $element){
+        echo $element.'<br>';
+    }
+echo '<hr>';
 }
