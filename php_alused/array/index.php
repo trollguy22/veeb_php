c535f14ea90be18b065bbbb402c017a7bf6f7b8d
trollguy22 <joel.purasson@khk.ee>
2019-02-25 10:15:16 +0200

    tabel

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -1,43 +1,86 @@
 <?php
-$peppa = array(
-    'nimi' => 'Peppa',
-    'sugu' => 'naine',
-    'vanus' => 4,
-    'pikkus' => 1.04
+$ajalugu = array(
+    'nimi' => 'Eesti side ja interneti 100 aastat',
+    'autor' => 'Jaak Ulman',
+    'zanr' => 'ajalugu',
+    'aasta' => 2019,
+    'hind' => 15
 );
-$george = array (
-    'nimi' => 'George',
-    'sugu' => 'mees',
-    'vanus' => 2,
-    'pikkus' => 0.95
+$IT = array(
+    'nimi' => 'Infotehnoloogia käsiraamat koolidele ja iseõppijatele II',
+    'autor' => 'Jaak Pihlau',
+    'zanr' => 'IT',
+    'aasta' => 1998,
+    'hind' => 10
+);
+$liha = array(
+    'nimi' => 'Lihtsad liharoad',
+    'autor' => 'Marika Vingissar',
+    'zanr' => 'kokandus',
+    'aasta' => 2018,
+    'hind' => 5.9
 );
 
-$porsad[0][] = 'punane';
-$porsad[1][] = 'sinine';
+echo  '<pre>';
+print_r($ajalugu);
+echo '</pre>';
 
-$porsad = array();
-$porsad['peppa'] = $peppa;
-$porsad['george'] = $george;
+echo  '<pre>';
+print_r($IT);
+echo '</pre>';
 
-foreach ($peppa as $nimi => $vaartus) {
-    echo $nimi.' - '.$vaartus.'<br>';
-}
+echo  '<pre>';
+print_r($liha);
+echo '</pre>';
+echo '<br>';
+?>
 
-echo '<hr>';
-$porsad['peppa']['lemmik_varv'] = 'punane';
-$porsad['george']['lemmik_varv'] = 'sinine';
-
-foreach ($george as $nimi => $vaartus) {
-    echo $nimi . ' - ' . $vaartus . '<br>';
-
-foreach ($porsad as $porsaseNimi=>$porsaseandmed)
-    echo '<b>' . $porsaseNimi . '</b><br>';
-}
-    foreach ($prosaseAndmed as $nimetus=>$vaartus)
-    {
-        echo '<dd>'.$nimetus.' - '.$vaartus.'<dd>';
-}
-
-//echo  '<pre>';
-//print_r($porsad);
-//echo '</pre>'
+<table>
+    <tbody>
+    <tr>
+        <td></td>
+        <td>raamat_1</td>
+        <td>raamat_2</td>
+        <td>raamat_3</td>
+    </tr>
+    <tr>
+        <td>nimi</td>
+        <td>Eesti side ja interneti 100 aastat</td>
+        <td>Infotehnoloogia käsiraamat koolidele ja iseõppijatele II</td>
+        <td>
+            <table>
+                <tbody>
+                <tr>
+                    <td>Lihtsad liharoad</td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td>autor</td>
+        <td>Jaak Ulmann</td>
+        <td>Jaak Pihlau</td>
+        <td>Marika Vingissar</td>
+    </tr>
+    <tr>
+        <td>zanr</td>
+        <td>Ajalugu</td>
+        <td>IT</td>
+        <td>kokandus</td>
+    </tr>
+    <tr>
+        <td>aasta</td>
+        <td>2019</td>
+        <td>1998</td>
+        <td>2018</td>
+    </tr>
+    <tr>
+        <td>hind</td>
+        <td>15</td>
+        <td>10</td>
+        <td>5.9</td>
+    </tr>
+    </tbody>
+</table>
+<p></p>
--- php_alused/array/nt1.php
+++ php_alused/array/nt1.php
@@ -1,11 +1,42 @@
 <?php
-$hinded = array(
-    'kehv' => 2,
-    'rahuldav' => 3,
-    'hea' => 4,
-    'väga hea' => 5
+$peppa = array(
+    'nimi' => 'Peppa',
+    'sugu' => 'naine',
+    'vanus' => 4,
+    'pikkus' => 1.04
 );
-foreach($hinded as $hinnang => $hinne){
-    echo "<li>Hinne: $hinne ($hinnang)</li>";
+$george = array(
+    'nimi' => 'George',
+    'sugu' => 'mees',
+    'vanus' => 2,
+    'pikkus' => 0.95
+);
+foreach ($peppa as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
 }
-?>
+echo '<hr>';
+foreach ($george as $nimi=>$vaartus){
+    echo $nimi.' - '.$vaartus.'<br>';
+}
+echo '<hr>';
+echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
+echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
+echo '<hr>';
+$porsad = array();
+$porsad['peppa'] = $peppa;
+$porsad['george'] = $george;
+$porsad['peppa']['lemmik varv'] = 'punane';
+$porsad['george']['lemmik varv'] = 'sinine';
+foreach ($porsad as $porsaseNimi=>$porsaseAndmed){
+    if($porsaseAndmed['sugu'] == 'naine'){
+        echo '<p style="color: red">';
+    } else {
+        echo '<p style="color: blue">';
+    }
+    echo '<b>'.$porsaseNimi.'</b></p>';
+    echo '<ul>';
+    foreach ($porsaseAndmed as $nimetus=>$vaartus){
+        echo '<li>'.$nimetus.' - '.$vaartus.'</li>';
+    }
+    echo '</ul>';
+}
