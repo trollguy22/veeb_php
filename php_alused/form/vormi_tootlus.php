75755e385cb6783246e82b513147fb646e0279c5
trollguy22 <joel.purasson@khk.ee>
2019-02-25 13:13:34 +0200

    ruumalade arvutamine

--- php_alused/form/vormi_tootlus.php
+++ php_alused/form/vormi_tootlus.php
@@ -2,16 +2,15 @@
 extract($_GET);
 //$nimi = $_GET['nimi'];
 //$parool = $_GET['parool'];
-foreach ($_GET as $nimetus => $vaartus) {
-    if(strlen($_GET[$nimetus]) == 0) {
-        header('Location: index.html');
-    }
-}
-if(strlen($nimi) == 0 or strlen($parool) == 0){
-    header('Location: index.html');
-    exit;
-} else {
+
     foreach ($_GET AS $nimetus => $vaartus){
         echo $nimetus.' => '.$vaartus.'<br>';
-    }
 }
+$silRuumala = (3.14 * $raadius * $raadius * $korgus);
+echo 'Silindri ruumala on '.$silRuumala.'<br>';
+
+$kooRuumala = (3.14 * $raadius * $raadius * $korgus * 0.3);
+echo 'Koonuse ruumala on '.$kooRuumala.'<br>';
+
+$keraRuumala = (1.3 * 3.14 * $raadius * $raadius * $raadius);
+echo 'Kera ruumala on '.$keraRuumala.'<br>';
