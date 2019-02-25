c894eed5253f09cec209f52e13a42329fd19f0c5
trollguy22 <joel.purasson@khk.ee>
2019-02-25 12:35:39 +0200

    strlen

--- php_alused/form/vormi_tootlus.php
+++ php_alused/form/vormi_tootlus.php
@@ -1,7 +1,15 @@
 <?php
-EXTRACT($_GET);
+extract($_GET);
 //$nimi = $_GET['nimi'];
 //$parool = $_GET['parool'];
-
-echo 'Tere '.$nimi.'!<br>';
-echo 'Sinu parooliks on '.$parool;
+foreach ($_GET as $nimetus => $vaartus) {
+    if(strlen($_GET[$nimetus]) == 0) {
+        header('Location: index.html');
+    }
+}
+if(strlen($nimi) == 0 or strlen($parool) == 0){
+    header('Location: index.html');
+} else {
+    echo 'Tere ' . $nimi . '!<br>';
+    echo 'Sinu parooliks on ' . $parool;
+}
