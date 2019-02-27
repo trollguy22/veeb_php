db3f5081f1779aa4619d26844cd7eb3dd17f5ef0
trollguy22 <joel.purasson@khk.ee>
2019-02-27 13:25:41 +0200

    ruumalade arvutamine parandatud

--- php_alused/form/vormi_tootlus.php
+++ php_alused/form/vormi_tootlus.php
@@ -1,16 +1,69 @@
 <?php
-extract($_GET);
-//$nimi = $_GET['nimi'];
-//$parool = $_GET['parool'];
-
-    foreach ($_GET AS $nimetus => $vaartus){
-        echo $nimetus.' => '.$vaartus.'<br>';
+// formi sisu
+function vorm(){
+    return '
+    <form method="get" action="'.$_SERVER['PHP_SELF'].'">
+<h3>Kera</h3>
+<div>
+    <div>
+        <label>Kera</label>
+        <input type="text" placeholder="kera raadius" name="keraRaadius">
+    </div>
+</div>
+<h3>Silinder</h3>
+<div>
+    <div>
+        <label>Silinder</label>
+        <input type="text" placeholder="silindri raadius" name="silindriRaadius">
+    </div>
+    <div>
+        <label>Silinder</label>
+        <input type="text" placeholder="silindri kõrgus" name="silindriKorgus">
+    </div>
+</div>
+<h3>Koonus</h3>
+<div>
+    <div>
+        <label>Koonus</label>
+        <input type="text" placeholder="koonuse raadius" name="koonuseRaadius">
+    </div>
+    <div>
+        <label>Silinder</label>
+        <input type="text" placeholder="koonuse kõrgus" name="koonuseKorgus">
+    </div>
+</div>
+<div>
+    <button type="submit">Submit</button>
+</div>
+</form>
+';
 }
-$silRuumala = (3.14 * $raadius * $raadius * $korgus);
-echo 'Silindri ruumala on '.$silRuumala.'<br>';
-
-$kooRuumala = (3.14 * $raadius * $raadius * $korgus * 0.3);
-echo 'Koonuse ruumala on '.$kooRuumala.'<br>';
-
-$keraRuumala = (1.3 * 3.14 * $raadius * $raadius * $raadius);
-echo 'Kera ruumala on '.$keraRuumala.'<br>';
+// ruumalate arvutused
+function keraRuumala($keraRaadius){
+    return 4/3 * pi() * pow($keraRaadius, 3);
+}
+function koonuseRuumala($koonuseRaadius, $koonuseKorgus){
+    return 1/3 * pi() * pow($koonuseRaadius, 2) * $koonuseKorgus;
+}
+function silindriRuumala($silindriRaadius, $silindriKorgus){
+    return pi() * pow($silindriRaadius, 2) * $silindriKorgus;
+}
+// andmete väljastamine
+function valjasta($ruumala){
+    return round($ruumala, 2).' cm<sup>3</sup><br>';
+}
+// väljastame vorm
+echo vorm();
+// andmete kontroll ja töötlus
+if(count($_GET) != 0) {
+    foreach ($_GET as $nimetus => $vaartus) {
+        if (strlen($_GET[$nimetus]) == 0) {
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit;
+        }
+    }
+// vormist andmed töötlus
+    echo 'Kera ruumala on ' . valjasta(keraRuumala($_GET['keraRaadius']));
+    echo 'Silindri ruumala on ' . valjasta(silindriRuumala($_GET['silindriRaadius'], $_GET['silindriKorgus']));
+    echo 'Koonuse ruumala on ' . valjasta(koonuseRuumala($_GET['koonuseRaadius'], $_GET['koonuseKorgus']));
+}
