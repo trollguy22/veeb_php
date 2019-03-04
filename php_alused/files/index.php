50bdeb3e89564e3739c8792eacaf9cba784c49b3
trollguy22 <joel.purasson@khk.ee>
2019-03-04 09:27:06 +0200

    algne form hinna võrdlemiseks

--- php_alused/files/index.php
+++ php_alused/files/index.php
@@ -1,24 +1,63 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+<body>
+<form method="get" action="'.$_SERVER['PHP_SELF'].'">
+    <select name="sorteeri">
+        <option value="odavam ette">odavam ette</option>
+        <option value="kallim ette">kallim ette</option>
+    </select>
+    <input type="submit" name="sorteeri">
+</form>
 
-function failiKontroll($failiNimi) {
-    if(is_file($failiNimi) and file_exists($failiNimi) and is_readable($failiNimi)){
+</body>
+</html>
+<?php
+function failiKontroll($failiNimi){
+    if(file_exists($failiNimi) and is_file($failiNimi) and is_readable($failiNimi)){
         return true;
     }
     return false;
 }
 
 function loeFailist($failiNimi){
+    $raamatud = array();
+    $nimetused = array(
+        'nimetus',
+        'autor',
+        'keel',
+        'lk',
+        'hind'
+    );
     if(failiKontroll($failiNimi)){
         $fp = fopen($failiNimi, 'r');
+        $raamat = array();
         while(!feof($fp)){
             $rida = fgets($fp);
-            if(trim($rida) == ''){
-                echo '===<br>';
-
+            if(trim($rida) != ''){
+                $raamat[] = $rida;
             } else {
-                echo $rida.'<br>';
+                $raamat = array_combine($nimetused, $raamat);
+                $raamatud[] = $raamat;
+                $raamat = array();
             }
         }
     }
+    return $raamatud;
 }
-loefailist('raamatud.txt');
+function tabelFailist($failiNimi){
+    if(failiKontroll($failiNimi)) {
+        $fp = fopen($failiNimi, 'r');
+        echo '<table border="1">';
+        echo '<tr>';
+        while (!feof($fp)) {
+            $rida = fgets($fp);
+            if(trim($rida) != ''){
+                echo '<td>'.$rida.'</td>';
+            } else {
+                echo '</tr>';
+            }
+        }
+        echo '</table>';
+    }
+}
+tabelFailist('raamatud.txt');
