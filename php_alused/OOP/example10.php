6b1e6e7fd370e160e83f0234230e1276b90ed539
trollguy22 <joel.purasson@khk.ee>
2019-03-10 19:32:11 +0200

    type hinting

---
+++ php_alused/OOP/example10.php
@@ -0,0 +1,17 @@
+<?php
+function calcNumMilesOnFullTank(array $models)
+{
+    foreach($models as $item)
+    {
+        echo $carModel = $item[0];
+        echo " : ";
+        echo $numberOfMiles = $item[1] * $item[2];
+        echo "<br />";
+    }
+}
+$models = array(
+    array('Toyota', 12, 44),
+    array('BMW', 13, 41)
+);
+
+calcNumMilesOnFullTank($models);
