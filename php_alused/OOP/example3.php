0a49d87b11c887c2a72dafe11d4112450f1381c4
trollguy22 <joel.purasson@khk.ee>
2019-03-10 18:57:56 +0200

    public ja private

--- php_alused/OOP/example3.php
+++ php_alused/OOP/example3.php
@@ -1,4 +1,30 @@
 <?php
 class Car {
+
     public $tank;
+
+    // Add gallons of fuel to the tank when we fill it.
+    public function fill($float)
+    {
+        $this-> tank += $float;
+
+        return $this;
+    }
+
+    // Substract gallons of fuel from the tank as we ride the car.
+    public function ride($float)
+    {
+        $miles = $float;
+        $gallons = $miles/50;
+        $this-> tank -= ($gallons);
+
+        return $this;
+    }
 }
+
+
+$bmw = new Car();
+
+$tank = $bmw -> fill(10) -> ride(40) -> tank;
+
+echo "The number of gallons left in the tank: " . $tank . " gal.";
